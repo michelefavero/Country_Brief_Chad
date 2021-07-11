d90c314ce24a527d8874130b4e913d3da723134e
Michele Favero <70494136+michelefavero@users.noreply.github.com>
2021-07-11 11:04:07 +0200

    Update Country Brief Chad.R

--- Country Brief Chad.R
+++ Country Brief Chad.R
@@ -3,22 +3,9 @@ library(WDI)
 ?WDI
 
 
-
-WDI(
-  country = "TCD",
-  indicator = c("SH.STA.MMRT", "SH.DYN.MORT.MA", "NY.GSR.NFCY.CD"),
-  start = 1960,
-  end = 2020,
-  extra = FALSE,
-  cache = NULL,
-  latest = NULL,
-  language = "en"
-)
-
-
 Chad_1 <- WDI(
   country = "TCD",
-  indicator = c("SP.RUR.TOTL.ZG", "SP.DYN.LE00.MA.IN", "SP.POP.0014.TO.ZS", "SP.POP.2529.MA.5Y", "SP.POP.6569.MA.5Y", "SH.STA.MMRT", "SH.DYN.MORT.MA", "NY.GSR.NFCY.CD"),
+  indicator = c("SP.RUR.TOTL.ZG", "SP.DYN.LE00.IN", "SP.POP.0014.TO.ZS", "SH.STA.MMRT", "SH.DYN.MORT.MA"),
   start = 1960,
   end = 2020,
   extra = FALSE,
@@ -26,7 +13,6 @@ Chad_1 <- WDI(
   latest = NULL,
   language = "en"
 )
-
 
 
 Chad_1 %>%
@@ -34,7 +20,7 @@ Chad_1 %>%
   filter(country == "Chad")
 
 
-ggplot(Chad_1, aes(x = year, y = SP.RUR.TOTL.ZG, color = SP.DYN.LE00.MA.IN)) +
+ggplot(Chad_1, aes(x = year, y = SP.RUR.TOTL.ZG, color = SP.DYN.LE00.IN)) +
   geom_line() +
   geom_point() +
   labs(x = "Years",
@@ -47,38 +33,21 @@ ggplot(Chad_1, aes(x = year, y = SP.RUR.TOTL.ZG, color = SP.DYN.LE00.MA.IN)) +
 
 
 
-# Global Peace Index
+# Peace Index
 
 
-Global_Peace_Index <- c("Peacekeeping", "Safety and security", "Political stability", "People protected")
+Peace_Index <- c("Peacekeeping", "Safety and security", "Political stability", "People protected")
 
 Measure <- c("2.47", "2.93", "1.50", "1.21")
 
-GPI <- data.frame(Global_Peace_Index, Measure)
+GPI <- data.frame(Peace_Index, Measure)
 GPI
 
-ggplot(GPI, aes(x = Global_Peace_Index, y = Measure)) +
-  geom_bar(stat = "identity", fill = "steelblue") +
-  theme_minimal() +
-  labs(title = "Global Peace Index",
-       x = "4 primary indicators",
-       y = "Current scale of investments (from 1 to 5)") 
-
-
-
-ggplot(GPI, aes(x = Global_Peace_Index, y = Measure)) +
+ggplot(GPI, aes(x = Peace_Index, y = Measure)) +
   geom_bar(stat = "identity", fill = "steelblue") +
   theme_minimal() +
   labs(title = "Global Peace Index",
        x = "4 primary indicators",
        y = "Current scale of investments (from 1 to 5)") +
   coord_flip()
-
-
-
-
-
-
-
-
 
